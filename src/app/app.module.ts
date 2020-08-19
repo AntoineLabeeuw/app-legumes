09df0ee05d08fd5885c365915aa020f1f98b54f2
Léo SENIQUE <leo.senique@gmail.com>
2020-08-19 11:06:27 +0200

    Barre de navigation #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { Route, RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+const ROUTES: Routes = [
+  //{ path: 'accueil', component: AccueilComponent},
+  //{ path: 'galerie', component: GalerieComponent},
+  //{ path: 'apropos', component: AproposComponent},
+  //{ path: '', pathMatch: 'full', redirectTo: '/accueil' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
