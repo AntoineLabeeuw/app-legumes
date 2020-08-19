f98c1b057051b11c4de562c95a366baddd7ca96e
Gaëlle BROSSAS <gaellebrossas@orange.fr>
2020-08-19 11:40:40 +0200

    mock et photo légumes

---
+++ src/app/mock/legume.mock.ts
@@ -0,0 +1,49 @@
+import { Legume } from './../models/legume';
+
+export const artichaut: Legume = {
+nom: "Artichaut",
+description: "De la famille des astéracées, comme le chardon sauvage, la culture de artichaut est répandu en Grèce depuis des millénaire et est arrivée en France au 16ème siècle.",
+photoUrl: "./assets/images/artichaut.JPG",
+};
+
+export const carotte: Legume = {
+  nom: "Carotte",
+  description: "Plante herbacée bisannuelle de la famille des apiacées, seule la racine riche en carotène est consommée.",
+  photoUrl: "./assets/images/carotte.JPG"
+  };
+
+export const citrouille: Legume = {
+  nom: "Citrouille",
+  description: "bla",
+  photoUrl: "./assets/images/citrouille.JPG"
+  };
+
+export const courgette: Legume = {
+  nom: "Courgette",
+  description: "Cucurbitacée pouvant pesée jusqu'à 50kg qui se consomme principalement en automne.",
+  photoUrl: "./assets/images/courgette.JPG"
+  };
+
+export const bettrave: Legume = {
+  nom: "Bettave",
+  description: "Légume-racine que l'on trouve de mai à octobre, la bettravue est aussi utilisée pour la production de sucre.",
+  photoUrl: "./assets/images/bettrave.JPG"
+  };
+
+export const pommeDeTerre: Legume = {
+  nom: "Pomme de terre",
+  description: "Tubercule comestible de la famille des solanacées. Sa culture est l'une des plus répandue",
+  photoUrl: "./assets/images/patate.JPG"
+  };
+
+export const tomate: Legume = {
+  nom: "Tomate",
+  description: "Plante de la famille des solanacées originaire d'Amérique, la tomate se retrouve dans la gastronomie de nombreux pays.",
+  photoUrl: "./assets/images/tomate.JPG"
+  };
+
+export const topinambour: Legume = {
+  nom: "Topinambour",
+  description: "Considéré comme un légume ancien, c'est une plante vivace de la famille des astéracées appartenant au même genre que le tournesol et cultivé pour ses tubercules.",
+  photoUrl: "./assets/images/topinambour.JPG"
+  };
