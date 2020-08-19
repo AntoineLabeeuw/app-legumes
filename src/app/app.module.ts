8666a761fd1e526b59edfb663602f44dac643bdd
Antoine LABEEUW <antoine.labeeuw@gmail.com>
2020-08-19 11:07:29 +0200

    create basic apropos page, #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AproposComponent } from './apropos/apropos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AproposComponent
   ],
   imports: [
     BrowserModule,
